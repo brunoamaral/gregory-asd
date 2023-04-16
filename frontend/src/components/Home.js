1a657e3a55d53f4c7f0f6daa7b0587e0c54e5965
Bruno Amaral <mail@brunoamaral.eu>
2023-04-16 17:08:59 +0100

    don't load nameform, yet.

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 // ./components/Home.js
 import { ArticlesList } from './Article';
 import Nav from './Nav';
-import NameForm from './NameForm';
+// import NameForm from './NameForm';
 const Home = () => {
 	return (
 		<>
